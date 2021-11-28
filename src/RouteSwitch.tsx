75a75abf88425b820277f5759298e1a61302e768
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-11-28 17:48:06 +0100

    Add Nav bar (non-functional)

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import App from "./Components/App";
+import Nav from "./Components/Nav";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<App />} />{" "}
       </Routes>
