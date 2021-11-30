c7d7c752ba880054dadec2dfd382f7c3bbd3fe87
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-11-30 18:53:19 +0100

    add footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,14 @@
+import "../Styles/Nav.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer beige-area">
+      <p>Jobs</p>
+      <p>FAQ</p>
+      <p>Privacy Policy</p>
+      <p>Shipping Information</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./Components/App";
+import Footer from "./Components/Footer";
 import Nav from "./Components/Nav";
 
 const RouteSwitch = () => {
@@ -7,8 +8,9 @@ const RouteSwitch = () => {
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<App />} />{" "}
+        <Route path="/" element={<App />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
