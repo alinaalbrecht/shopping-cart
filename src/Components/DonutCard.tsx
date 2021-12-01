6b0fed743fe4202e02e405de0e0f3421700d92d8
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-12-01 19:05:58 +0100

    map over donuts prop to display donut cards

--- src/Components/DonutCard.tsx
+++ src/Components/DonutCard.tsx
@@ -1,11 +1,37 @@
 import "../Styles/Shop.css";
 
 interface DonutCardProps {
-  donuts: object[];
+  donuts: {
+    name: string;
+    price: string;
+    image: string;
+  }[];
 }
 
-const DonutCard: React.FC<DonutCardProps> = () => {
-  return <div className="donut-item container"></div>;
+const DonutCard: React.FC<DonutCardProps> = (props) => {
+  return (
+    <>
+      {props.donuts.map((donut) => {
+        return (
+          <div className="donut-item container">
+            <div className="donut-image">
+              <img src={donut.image} alt="" />
+            </div>
+            <div className="donuts-added inactive" data-quantity="0"></div>
+            <p className="donut-name">{`${donut.name} ${donut.price}`}</p>
+            <div className="quantity-selector" data-quantity="0">
+              <div className="subtract">-</div>
+              <div className="quantity inactive">0</div>
+              <div className="add">+</div>
+            </div>
+            <button className="small-button" data-type="add-to-basket">
+              Add to Basket
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default DonutCard;
